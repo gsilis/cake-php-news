bd3184002741c3fbd4447d4d5f49987fabfd8a77
Girts Silis <girts.silis@gmail.com>
2013-08-18 14:50:40 -0400

    Change default cake salt and seed

--- app/Config/core.php
+++ app/Config/core.php
@@ -194,12 +194,12 @@
 /**
  * A random string used in security hashing methods.
  */
-	Configure::write('Security.salt', 'DYhG93b0qyJfIxfs2guVoUubWwvniR2G0FgaC9mi');
+	Configure::write('Security.salt', 's0meisa-;oassadfj.asdfdf*klasdf7&5lkas^@%');
 
 /**
  * A random numeric string (digits only) used to encrypt/decrypt strings.
  */
-	Configure::write('Security.cipherSeed', '76859309657453542496749683645');
+	Configure::write('Security.cipherSeed', '120985627083452918575023948571');
 
 /**
  * Apply timestamps with the last modified time to static assets (js, css, images).
